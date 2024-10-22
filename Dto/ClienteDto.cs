ffed6659fde78d5761a4c69e603b6fe94faabbd1
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2024-10-22 01:02:26 -0300

    desativando warning nullable

--- Dto/ClienteDto.cs
+++ Dto/ClienteDto.cs
@@ -1,21 +1,21 @@
-using ClientListApi.Models;
-
 namespace ClientListApi.Dto
 {
     public class ClienteDto
     {
-        public long Id { get; set; }
-        public  string Nome { get; set; }
-        public  string Telefone { get; set; }
-        public  bool Status { get; set; }
-        public  DateOnly DataNascimento { get; set; }
-        public  long Cep { get; set; }
-        public  string Endereco { get; set; }
-        public  string NumeroEndereco { get; set; }
         public string? Complementeo { get; set; }
-        public  string Bairro { get; set; }
-        public  string Cidade { get; set; }
-        public  string Estado { get; set; }
-        public  long VendedorId { get; set; }
+#nullable disable
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Status { get; set; }
+        public DateOnly DataNascimento { get; set; }
+        public long Cep { get; set; }
+        public string Endereco { get; set; }
+        public string NumeroEndereco { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public long VendedorId { get; set; }
+        public string Vendedor { get; set; }
     }
 }
