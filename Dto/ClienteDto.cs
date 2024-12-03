05fdee902a7d9bf80c1ab04a048ac33c471faae3
kennedyAlvess <kennedy.pereira@alunos.ufersa.edu.br>
2024-12-02 22:03:58 -0300

    fix: Ajustando avisos de nullables

--- Dto/ClienteDto.cs
+++ Dto/ClienteDto.cs
@@ -5,16 +5,16 @@ namespace ClientListApi.Dto
     public class ClienteDto
     {
         public long Id { get; set; }
-        public string Nome { get; set; }
-        public string Telefone { get; set; }
+        public string Nome { get; set; } = "";
+        public string Telefone { get; set; } = "";
         public bool Status { get; set; }
         public DateOnly DataNascimento { get; set; }
         public long Cep { get; set; }
-        public string Endereco { get; set; }
-        public string NumeroEndereco { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
+        public string Endereco { get; set; } = "";
+        public string NumeroEndereco { get; set; } = "";
+        public string Bairro { get; set; } = "";
+        public string Cidade { get; set; } = "";
+        public string Estado { get; set; } = "";
         public string? Complementeo { get; set; }
         public long VendedorId { get; set; }
 
--- Models/ClienteModel.cs
+++ Models/ClienteModel.cs
@@ -5,19 +5,19 @@ namespace ClientListApi.Models
     public class ClienteModel
     {
         public long Id { get; set; }
-        public string Nome { get; set; }
-        public string Telefone { get; set; }
+        public required string Nome { get; set; }
+        public required string Telefone { get; set; }
         public bool Status { get; set; }
         public DateOnly DataNascimento { get; set; }
         public long Cep { get; set;}
-        public string Endereco { get; set; }
-        public string NumeroEndereco { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set;}
-        public string Estado { get; set;}
+        public required string Endereco { get; set; }
+        public required string NumeroEndereco { get; set; }
+        public required string Bairro { get; set; }
+        public required string Cidade { get; set;} 
+        public required string Estado { get; set;} 
         public string? Complementeo { get; set; }
         public long VendedorId { get; set; }
         [JsonIgnore]
-        public VendedorModel Vendedor { get; set; }
+        public VendedorModel Vendedor { get; set; } = null!;
     }
 }
